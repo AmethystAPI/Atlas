a1771da18566ba10504466b2e422f562ccb10e45
Adrian <adrian.olbrich1@gmail.com>
2024-02-17 19:18:57 +0100

    init

--- BlockNav/src/dllmain.h
+++ BlockNav/src/dllmain.h
@@ -4,12 +4,6 @@
 #include <amethyst/InputManager.h>
 #include <amethyst/events/EventManager.h>
 
-#include "hwinfo/utils/PCIMapper.h"
-#include <hwinfo/hwinfo.h>
-#include <iomanip>
-#include <iostream>
-#include <vector>
-
 #define ModFunction extern "C" __declspec(dllexport)
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
--- BlockNav/src/recorder/hardware_recorder/HardwareRecorder.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by adria on 17/02/2024.
-//
-
-#include <hwinfo/hwinfo.h>
-
-class HardwareRecorder {
-};
