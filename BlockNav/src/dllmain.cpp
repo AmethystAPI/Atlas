4f5c433fb0d11bbe35b0967c8ff55c07a87465c9
Adrian <adrian.olbrich1@gmail.com>
2024-02-18 14:22:40 +0100

    removed f3 in minimap

--- BlockNav/src/dllmain.cpp
+++ BlockNav/src/dllmain.cpp
@@ -1,11 +1,8 @@
 ﻿#include "dllmain.h"
-#include "recorder/ps_recorder/psRecorder.h"
-#include "ui/f3/f3Renderer.h"
 #include "ui/minimap/minimapRenderer.h"
 
 ClientInstance* client = nullptr;
 bool map_open = false;
-bool f3_open = false;
 
 // Ran when the mod is loaded into the game by AmethystRuntime
 ModFunction void Initialize(HookManager* hookManager, Amethyst::EventManager* eventManager, InputManager* inputManager)
@@ -18,16 +15,11 @@ ModFunction void Initialize(HookManager* hookManager, Amethyst::EventManager* ev
     inputManager->RegisterInput("use_map", 0x4D);
     inputManager->AddButtonDownHandler("use_map", onUseMap);
 
-    inputManager->RegisterInput("use_f3", 0x72);
-    inputManager->AddButtonDownHandler("use_f3", &onUseF3);
-
-    //// Add a listener to a built-in amethyst event
+    // Add a listener to a built-in amethyst event
     eventManager->onStartJoinGame.AddListener(&OnStartJoinGame);
     eventManager->onRequestLeaveGame.AddListener(onRequestLeaveGame);
 
     eventManager->onRenderUI.AddListener(&onRenderUi);
-
-    psRecorder::registerEventHandlers(eventManager);
 }
 
 // Subscribed to amethysts on start join game event in Initialize
@@ -43,7 +35,6 @@ void onRequestLeaveGame()
     Log::Info("The player has top_left the game!");
 
     map_open = false;
-    f3_open = false;
 }
 
 void onRenderUi(ScreenView* screenView, MinecraftUIRenderContext* uiRenderContext)
@@ -52,10 +43,6 @@ void onRenderUi(ScreenView* screenView, MinecraftUIRenderContext* uiRenderContex
         if (map_open) {
             MiniMapRenderer::Renderer(screenView, uiRenderContext, client);
         }
-
-        if (f3_open) {
-            f3Renderer::Renderer(screenView, uiRenderContext, client);
-        }
     }
 }
 
@@ -63,10 +50,4 @@ void onUseMap(FocusImpact focus, IClientInstance clientInstance)
 {
     map_open = !map_open;
     Log::Info("Map");
-}
-
-void onUseF3(FocusImpact focus, IClientInstance clientInstance)
-{
-    f3_open = !f3_open;
-    Log::Info("F3");
 }
